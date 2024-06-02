54c267fe8270ae96c7c55e241a401a9961c1eded
MatveiKylosov <matveikyl@mail.ru>
2024-06-02 23:43:31 +0500

    реализовано сохранение графика в базу данных

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,5 +42,49 @@ namespace PermDynamics
             InitializeComponent();
             OpenPages(pages.main);
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;password=;database=chart";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string tableName = "chart_" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string createTableQuery = $"CREATE TABLE {tableName} (id INT AUTO_INCREMENT PRIMARY KEY, value DOUBLE)";
+
+                using (MySqlCommand createTableCommand = new MySqlCommand(createTableQuery, connection))
+                {
+                    createTableCommand.ExecuteNonQuery();
+                }
+
+                Console.WriteLine($"Таблица {tableName} успешно создана.");
+
+                string insertDataQuery = $"INSERT INTO {tableName} (value) VALUES ";
+
+                List<MySqlParameter> parameters = new List<MySqlParameter>();
+                for (int i = 0; i < pointsInfo.Count; i++)
+                {
+                    string paramName = $"@value{i}";
+                    insertDataQuery += $"({paramName})";
+
+                    if (i < pointsInfo.Count - 1)
+                        insertDataQuery += ", ";
+
+                    parameters.Add(new MySqlParameter(paramName, pointsInfo[i].value));
+                }
+
+                using (MySqlCommand insertDataCommand = new MySqlCommand(insertDataQuery, connection))
+                {
+                    insertDataCommand.Parameters.AddRange(parameters.ToArray());
+                    insertDataCommand.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+
     }
 }
